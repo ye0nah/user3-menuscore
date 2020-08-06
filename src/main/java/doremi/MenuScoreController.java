037f87757eea4c58e0055622ff0f67d62ce75879
ye0nah <pyangru@naver.com>
2020-08-06 11:22:46 +0900

    Update MenuScoreController.java

--- src/main/java/doremi/MenuScoreController.java
+++ src/main/java/doremi/MenuScoreController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
  public class MenuScoreController {
   @Autowired
   MenuScoreRepository pointRepo;
-  @PostMapping("/menuscore/save")
+  @PostMapping("/menuscores/save")
   public MenuScore saved(@RequestBody MenuScore postPoint) {
 
    MenuScore point = new MenuScore();
@@ -34,7 +34,7 @@ import java.util.Optional;
    return point;
   }
 
-  @PostMapping("/menuscore/rate")
+  @PostMapping("/menuscores/rate")
   public MenuScore rated(@RequestBody MenuScore postPoint) {
 
    MenuScore point = new MenuScore();
@@ -59,7 +59,7 @@ import java.util.Optional;
   }
 
 
-  @PostMapping("/menuscore/cancel")
+  @PostMapping("/menuscores/cancel")
   public MenuScore cancelled(@RequestBody MenuScore postPoint) {
 
    MenuScore point = new MenuScore();
@@ -77,7 +77,7 @@ import java.util.Optional;
 
    return point;
   }
-   @RequestMapping(value = "/menuscore/select/{id}",
+   @RequestMapping(value = "/menuscores/select/{id}",
          method = RequestMethod.POST,
          produces = "application/json;charset=UTF-8")
  public void select(@PathVariable("id") Long id) {
